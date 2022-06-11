283fdd7c68e0e9bfb2760b4b366fad1da23efdb7
Gaurav Rajput <gauravrajput2771@gmail.com>
2022-06-11 17:12:12 -0400

    my all five link that i use mostly

--- taxtutilesss/taxtutilesss/urls.py
+++ taxtutilesss/taxtutilesss/urls.py
@@ -18,9 +18,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('file/', views.file,name='file'),
-
-
+    path('url/', views.url,name='url'),
 
 
 ]
--- taxtutilesss/taxtutilesss/views.py
+++ taxtutilesss/taxtutilesss/views.py
@@ -10,10 +10,13 @@
 #     return HttpResponse("it is our web page . you surfing any time here ")
 
 from django.http import HttpResponse
-def file(request):
-    f = open("gaurav2.txt", "r+")
 
 
-
-    return HttpResponse(f.read(),f.write("<h1>  Gaurav is good developer <\h1>"),)
+def url(request):
+    return HttpResponse('''<h1>         My all five links that i use mostly     </h1><a  
+    href="https://www.youtube.com/playlist?list=PLu0W_9lII9ah7DDtYtflgwMwpT3xmjXY9"> <h1>Dhango play list link</h1></a><a  
+    href="https://www.youtube.com/playlist?list=PLu0W_9lII9ahfRrhFcoB-4lpp9YaBmdCP"> <h1> Python oops playlist link</h1></a><a  
+    href="https://www.youtube.com/playlist?list=PL0b6OzIxLPbzf12lu5etX_vjN-eUxgxnr"> <h1> my sql  playlist link</h1></a><a  
+    href="https://www.youtube.com/playlist?list=PLu0W_9lII9agICnT8t4iYVSZ3eykIAOME"> <h1> my fav PYTHON playlist link</h1></a><a  
+    href="https://www.youtube.com/playlist?list=PLlasXeu85E9cQ32gLCvAvr9vNaUccPVNP"> <h1> My javascript playlist link</h1></a>''')
 
