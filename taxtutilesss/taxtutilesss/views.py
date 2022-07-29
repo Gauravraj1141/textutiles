55dc37f79d137a7c520506e187e92859800c6148
Gaurav Rajput <gauravrajput2771@gmail.com>
2022-07-29 16:29:31 +0530

    texttutiles file

--- taxtutilesss/taxtutilesss/views.py
+++ taxtutilesss/taxtutilesss/views.py
@@ -1,36 +1,67 @@
-#this file is created by me = Gaurav Rajput
-
-
-
-
-# def index(request):
-#
-#     return HttpResponse(" <h1> Hello Gaurav kse ho yar  how to prepare your python </h1>")
-#
-# def aboutus(request):
-#     return HttpResponse("it is our web page . you surfing any time here ")
-
+from django.shortcuts import render
 from django.http import HttpResponse
-from django.shortcuts import render # ye templates k liye import kiya h
-
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index2.html')
 
 
-    # return HttpResponse("<h1>Home</h1>")
+def analyze(request):
+    djtext = request.POST.get('text','default')
+    removepunc = request.GET.get('removepunc','off')
+    uppercase = request.GET.get('uppercase','off')
+    newlineremove = request.GET.get('newlineremove','off')
+    spaceremover = request.GET.get('spaceremover','off')
+    charcount = request.GET.get('charcount','off')
 
-def removepunc(request):
-    return render(request, 'removepunc.html')
-    # return HttpResponse("<h1>remove punc</h1>")
 
-def capitalizefirst(request):
-    return HttpResponse("<a href = '/' back> </a>")
+    if removepunc == "on":
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        analyzed = ""
+        for char in djtext:
+            if char not in punctuations:
+                analyzed =  analyzed + char
+        params = {'purpose': 'Removed Punctutaions', 'analyzed_text': analyzed}
+        return render(request, 'analyze2.html', params)
+    elif uppercase  == "on":
+        analyzed = ""
+        for char in djtext:
+            analyzed = analyzed + char
+            analyzed = analyzed.upper()
+        params = {'purpose': 'Upper case', 'analyzed_text': analyzed}
+        return render(request, 'analyze2.html', params)
 
-def newlineremove(request):
-    return HttpResponse("<h1>newlineremove</h1>")
+    elif newlineremove == "on":
+        analyzed = ""
+        for char in djtext:
+            if char != "\n" and char != "\r":
+                analyzed = analyzed + char
+        params = {'purpose': 'Newlineremove', 'analyzed_text': analyzed}
+        return render(request, 'analyze2.html', params)
 
-def spaceremover(request):
-    return HttpResponse("<h1>spaceremover</h1>")
-def charcount(request):
-    return HttpResponse("<h1>charcount</h1>")
+    elif spaceremover == "on":
+        analyzed = ""
+        for i , char  in enumerate(djtext):
+            if not(djtext[i]== " " and djtext[i+1] == " "):
+                analyzed = analyzed + char
+        params = {'purpose': 'space Remove', 'analyzed_text': analyzed}
+        return render(request, 'analyze2.html', params)
+
+    elif charcount == "on":
+
+        analyzed = 0
+        for char in djtext:
+            analyzed = analyzed + len(char)
+
+
+
+        params = {'purpose': 'Character Count', 'analyzed_text': analyzed}
+        return render(request, 'analyze2.html', params)
+
+
+
+    else:
+        return HttpResponse("error")
+
+
+
+
